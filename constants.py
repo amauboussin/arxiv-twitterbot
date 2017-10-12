7454031d1a535c31e7ae2f0521a41d84f4f3e6c5
Andrew Mauboussin <amaub217@gmail.com>
2017-10-11 20:35:50 -0400

    fix filepaths for cron job

--- constants.py
+++ constants.py
@@ -1,3 +1,4 @@
+import os
 
 TWEET_THRESHOLD = .5
 MAX_TWEET_LENGTH = 140  # holding out for 280
@@ -7,10 +8,12 @@ BASE_URL = 'http://export.arxiv.org/api/query'
 KARPATHY_CATEGORIES = ['cs.CV', 'cs.AI', 'cs.LG', 'cs.CL', 'cs.NE', 'stat.ML']
 BRUNDAGE_CATEGORIES = ['cs*', 'cond-mat.dis-nn', 'q-bio.NC', 'stat.CO', 'stat.ML']
 
-PICKLE_PATH = 'all_arxiv.pickle'
-TWEET_CSV_PATH = 'miles_links.csv'
-DICT_TRANSFORM_PATH = 'dictionary.json'
-MODEL_PATH = 'conv_weights.h5'
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+PICKLE_PATH = os.path.join(current_directory, 'all_arxiv.pickle')
+TWEET_CSV_PATH = os.path.join(current_directory, 'miles_links.csv')
+DICT_TRANSFORM_PATH = os.path.join(current_directory, 'dictionary.json')
+MODEL_PATH = os.path.join(current_directory, 'conv_weights.h5')
 
 QUERY_PAGE_SIZE = 500
 QUERY_WAIT_TIME = 10  # in seconds
