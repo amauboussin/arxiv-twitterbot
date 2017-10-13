4d40197d0e38ae0d6ac17fa45f8f87b16cdc9d00
Andrew Mauboussin <amaub217@gmail.com>
2017-10-12 22:23:13 -0400

    try sleeping before tweeting

--- job.py
+++ job.py
@@ -1,6 +1,9 @@
+from time import sleep
+
 from get_arxiv import check_for_update, update_arxiv
 from tweet_papers import tweet_latest_day
 
 if check_for_update():
     update_arxiv()
+    sleep(2)
     tweet_latest_day(dry_run=False)
