f72e677fe03b329d759fec9e18cb788656b1a1e6
root <root@Ghost-on-1404.052316-305>
2017-11-17 14:08:11 -0500

    update predictions pickle

--- constants.py
+++ constants.py
@@ -14,7 +14,7 @@ BRUNDAGE_CATEGORIES = ['cs*', 'cond-mat.dis-nn', 'q-bio.NC', 'stat.CO', 'stat.ML
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
 PICKLE_PATH = os.path.join(current_directory, 'all_arxiv.pickle')
-PAST_PREDICTIONS_PATH = os.path.join(current_directory, 'predictions.pickle')
+PAST_PREDICTIONS_PATH = os.path.join(current_directory, 'predictions2.pickle')
 TWEET_CSV_PATH = os.path.join(current_directory, 'miles_links.csv')
 DICT_TRANSFORM_PATH = os.path.join(current_directory, 'dictionary.json')
 MODEL_PATH = os.path.join(current_directory, 'conv_weights.h5')
